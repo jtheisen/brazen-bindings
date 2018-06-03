8f3f6078c04b4ccbd0572b75acf21e9e53233d21
Jens Theisen <pass-github@zokfotpik.de>
2018-06-03 13:22:21 +0200

    thrown out extra blueprint css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { render } from "react-dom";
 import { Demo } from "./demo";
 
+import "@blueprintjs/core/lib/css/blueprint.css";
+
 const App = () => (
   <div>
     <Demo />
